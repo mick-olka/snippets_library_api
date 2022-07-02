033e847312bacc36ac7ed764ead2f6d5f79f11b8
mick-olka <mickolka64@gmail.com>
2022-07-02 13:14:15 +0300

    update: mail sender

--- src/controller/Home/index.ts
+++ src/controller/Home/index.ts
@@ -1,9 +1,8 @@
 import { NextFunction, Request, Response } from 'express'
 
-import { createTransport } from 'nodemailer'
-
 import * as homeService from '@/service/home'
 import { catchAsync } from '@/utils/catchAsync'
+import { sendMail } from '@/utils/sendMail'
 
 export const getAppInfo = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
   const result = homeService.getAppInfo()
@@ -15,31 +14,7 @@ export const getMainPage = catchAsync(async (req: Request, res: Response, next: 
   res.render('index', { title: 'Main', arr: [1, 2, 4] })
 })
 
-export const sendMail = async (req: Request, res: Response) => {
-  const transporter = createTransport({
-    host: 'smtp-mail.outlook.com', // hostname
-    secure: false, // TLS requires secureConnection to be false
-    port: 587, // port for secure SMTP
-    tls: {
-      ciphers: 'SSLv3',
-    },
-    auth: {
-      user: 'srenik_brek@outlook.com',
-      pass: '7m7RXC2dKTE2DjE',
-    },
-  })
-  const mail = {
-    from: 'srenik_brek@outlook.com',
-    to: 'pavelbin29@gmail.com',
-    subject: 'Test',
-    text: 'test message',
-  }
-  transporter.sendMail(mail, (err, info) => {
-    if (err) {
-      console.log(err)
-    } else {
-      console.log(info.response)
-    }
-  })
+export const sendMailR = async (req: Request, res: Response) => {
+  await sendMail('hfueiwhfueiwhfueiw', 'pavelbin29@gmail.com')
   res.json({ message: 'Mail sent' })
 }
--- src/utils/sendMail.ts
+++ src/utils/sendMail.ts
@@ -1,4 +1,8 @@
+import handlebars from 'handlebars'
 import { createTransport } from 'nodemailer'
+
+import fs from 'fs'
+import path from 'path'
 
 export const sendMail = async (hash: string, email: string) => {
   const transporter = createTransport({
@@ -9,15 +13,22 @@ export const sendMail = async (hash: string, email: string) => {
       ciphers: 'SSLv3',
     },
     auth: {
-      user: 'srenik_brek@outlook.com',
-      pass: '7m7RXC2dKTE2DjE',
+      user: process.env.MAIL_USER,
+      pass: process.env.MAIL_PW,
     },
   })
+  const html = fs.readFileSync(path.resolve(__dirname, '../../views', 'mail.hbs'), 'utf8')
+  const template = handlebars.compile(html)
+  const data = {
+    link: process.env.FRONT_LINK + 'users/confirm/' + hash,
+  }
+  const htmlToSend = template(data)
   const mail = {
-    from: 'srenik_brek@outlook.com',
+    from: process.env.MAIL_USER,
     to: email,
     subject: 'Confirm email',
-    text: 'http://192.168.0.103:8000/users/confirm/' + hash,
+    //text: 'http://192.168.0.103:8000/users/confirm/' + hash,
+    html: htmlToSend,
   }
   return await transporter.sendMail(mail)
 }
