85b032ac39c656ee72325aeffe40cd137b98b466
mick-olka <mickolka64@gmail.com>
2022-07-06 08:45:44 +0100

    save

--- src/routes/Home/index.ts
+++ src/routes/Home/index.ts
@@ -8,10 +8,6 @@ router.get('/', homeController.getAppInfo)
 
 router.get('/index', homeController.getMainPage)
 
-<<<<<<< HEAD
-router.get('/mail', homeController.sendMailR)
-=======
 router.get('/mail/:mail', homeController.sendMailR)
->>>>>>> 17d4207a9c3f41626b9e6b8edb4826c37ac3e012
 
 export default router
